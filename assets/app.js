8c6f2ee6668b450b07788300da3fb1334b873d0c
abuse <xxxxx.abuse@gmail.com>
2025-08-25 13:58:11 +0500

    пофикшен :hover кнопок, проверка на существование email в регистрации, анимация в регистрации и логине, улучшена структура

--- assets/app.js
+++
@@ -1,6 +0,0 @@
-const terms = document.getElementById('terms');
-const submit = document.getElementById('submit');
-
-terms.addEventListener('change', (e) => {
-    submit.disabled = !e.currentTarget.checked;
-});
